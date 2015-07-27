d7caba8ff58ee6900361caef1fe322085b7431f8
leroi <leroidejesa@gmail.com>
2015-07-27 14:42:07 -0700

    initial commit

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
   // your community or team name to display on join page.
-  community: process.env.COMMUNITY_NAME || 'YOUR-TEAM-NAME',
+  community: process.env.COMMUNITY_NAME || 'epicodus-spr2015-ruby',
   // your slack team url (ex: socketio.slack.com)
-  slackUrl: process.env.SLACK_URL || 'YOUR-TEAM.slack.com',
+  slackUrl: process.env.SLACK_URL || 'epicodus-spr2015-ruby.slack.com',
   // access token of slack
   // You can generate it in https://api.slack.com/web#auth
   //
@@ -10,5 +10,5 @@ module.exports = {
   //   curl -X POST 'https://YOUR-SLACK-TEAM.slack.com/api/users.admin.invite' \
   //   --data 'email=EMAIL&token=TOKEN&set_active=true' \
   //   --compressed
-  slacktoken: process.env.SLACK_TOKEN || 'YOUR-ACCESS-TOKEN'
+  slacktoken: process.env.SLACK_TOKEN || 'xoxp-4943763046-4943763050-8271653905-86db85'
 };
